4f617f8d4d2518732e3bc473ab2a77811411f1dc
g16925mn <g16925mn@gm.tsuda.ac.jp>
2018-05-09 15:57:02 +0900

    テストケース

--- test/snakecamel/SnakecamelUnitTest.java
+++ test/snakecamel/SnakecamelUnitTest.java
@@ -7,8 +7,25 @@ import org.junit.Test;
 public class SnakecamelUnitTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		SnakeCamelUtil SCU=new SnakeCamelUtil();
+		String a,b;
+		a="AbcDefGhi";
+		b=SCU.snakeToCamelcase("abc_def_ghi");
+		assertEquals(a,b);
+		
+		
+		//fail("Not yet implemented");
 	}
+	public void test2() {
+		SnakeCamelUtil SCU=new SnakeCamelUtil();
+		String a="abc_def_ghi";
+		String b=SCU.snakeToCamelcase("AbcDefGhi");
+		assertEquals(a,b);
+		
+		
+		//fail("Not yet implemented");
+	}
+
 
 }
